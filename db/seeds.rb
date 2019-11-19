961c5a6a5618989253c6b58e1cbf06639bc66bad
Raul Fiori <raulproc1807@gmail.com>
2019-11-19 14:19:15 -0300

    implemented post controller and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+posts = Post.create([{ title: 'meu post', description: 'um post sobre coisas aleatórias da internet' },
+                     { title: 'politica', description: 'alfinetando todos que nao compartilham da minha opnião' },
+                     { title: 'minha comida', description: 'eu como coisas bonitas e preciso mostrar pro mundo' },
+                     { title: 'minha viagem', description: 'olha como vida é legal' }])
+
+Comment.create(content: 'não concordo, vou te alfinetar de volta', post: posts[1])
+Comment.create(content: 'maravilhoso.. sobrinho lindo da tia... s2..', post: posts.last)
